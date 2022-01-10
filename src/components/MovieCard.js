dbb7f2051f5ba687add7144de01427f60b11726b
develinu <iwlee.dev@gmail.com>
2022-01-10 19:04:19 +0900

    feat: 빈 카드 표출을 위한 스켈레톤 레이아웃 추가

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,56 +1,69 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 import '../style/MovieCard.scss'
 import Loader from './Loader'
 
 
-const MovieCard = ({ movie }) => {
+const MovieCard = ({ movie, empty }) => {
   const [imageLoading, setImageLoading] = useState(true)
 
-  if (movie?.Poster === 'N/A') {
-    setImageLoading(false)
-  } else {
-    const img = new Image()
-    img.src = movie?.Poster
-    img.onload = () => {
+  useEffect(() => {
+    if (empty || movie?.Poster === 'N/A') {
       setImageLoading(false)
+    } else {
+      const img = new Image()
+      img.src = movie?.Poster
+      img.onload = () => {
+        setImageLoading(false)
+      }
     }
-  }
+  }, [])
 
   return (
-    <Link 
-      to={`/movie/${movie?.imdbID}`}
-      className="movie">
-        {
-          imageLoading
-          ? <Loader scale=".5" absolute />
-          :
-            <>
-            <div
-              className="poster"
-              style={{ backgroundImage: `url(${movie?.Poster})` }}>
-              {
-                movie?.Poster === 'N/A'
-                ?
-                <>
-                OMDbAPI<br />
-                N/A
-                </>
-                : null
-              }
-            </div>
-            <div className="info">
-              <div 
+    empty 
+    ? 
+      <div className="movie">
+        <div className="poster"></div>
+        <div className="info">
+          <div className="poster"></div>
+          <div className="year"></div>
+          <div className="title"></div>
+        </div>
+      </div>
+    :
+      <Link 
+        to={`/movie/${movie?.imdbID}`}
+        className="movie">
+          {
+            imageLoading
+            ? <Loader scale=".5" absolute />
+            :
+              <>
+              <div
                 className="poster"
                 style={{ backgroundImage: `url(${movie?.Poster})` }}>
+                {
+                  movie?.Poster === 'N/A'
+                  ?
+                  <>
+                  OMDbAPI<br />
+                  N/A
+                  </>
+                  : null
+                }
               </div>
-              <div className="year">{movie?.Year}</div>
-              <div className="title">{movie?.Title}</div>
-            </div>
-            </>
-        }
-    </Link>
+              <div className="info">
+                <div 
+                  className="poster"
+                  style={{ backgroundImage: `url(${movie?.Poster})` }}>
+                </div>
+                <div className="year">{movie?.Year}</div>
+                <div className="title">{movie?.Title}</div>
+              </div>
+              </>
+          }
+      </Link>
   )
 }
 
