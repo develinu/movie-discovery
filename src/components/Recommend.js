63bf11a408e40e19ca0319c37969905bb7f17ad0
develinu <iwlee.dev@gmail.com>
2022-01-10 19:05:01 +0900

    feat: 추천 컴포넌트 구현

--- src/components/Recommend.js
+++ src/components/Recommend.js
@@ -1,44 +1,81 @@
 import React, { useEffect, useState } from 'react'
 
 import '../style/Recommend.scss'
-import { getMovies } from '../data/movieApi'
 import MovieCard from './MovieCard'
+import Loader from './Loader'
+import { getLikeMovies, getMovie } from '../data/movieApi'
 
 
 const Recommend = () => {
 
-  useEffect(() => {
-    getMovies()
-  }, [])
-
-  const name = ["이누", "대지", "가을", "동주"]
-  const [movie, setMovie] = useState({})
-  
-  let loading = true
+  const users = ["이누", "대지", "가을", "동주"] 
 
   return (
     <>
     {
-      name.map(n => <RecommendBelt name={n} />)
+      users.map((u, idx) => <RecommendBelt key={idx} userId={u} />)
     }
     </>
   )
 }
 
-const RecommendBelt = ({ name }) => {
+const RecommendBelt = ({ userId }) => {
+
+  const displayMovieCardCount = 5
+  const [loading, setLoading] = useState(true)
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    getLikeMovie()
+  }, [])
+  
+  const getLikeMovie = async () => {    
+    const _likeMovies = await getLikeMovies(userId)
+
+    const _movies = _likeMovies.data?.movies
+    if ( _movies && _movies.length > 0 ) {
+      const promises = _movies.map(async m => {
+        let _movie = await getMovieDetail(m)
+        return _movie.data ? _movie.data : {}
+      })
+      let _result = await Promise.all(promises)
+      setMovies(_result)
+    }
+
+    setLoading(false)
+  }
+
+  const getMovieDetail = async (movieId) => {
+    return await getMovie(movieId)
+  }
+
   return (
     <div className="recommend-belt">
-      <div className="title">
-        {name}님이 좋아하는 영화 리스트
-      </div>
+      {
+        loading
+        ? <Loader />
+        : 
+        <>
+        <div className="title">
+          {userId}님이 좋아하는 영화 리스트
+        </div>
 
-      <div className="recommend-list">
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-      </div>
+        <div className="recommend-list">
+          {
+            movies
+            ? movies.map(movie => {
+              return <MovieCard key={movie.imdbID} movie={movie}/>
+            })
+            : null
+          }
+          {
+            Array.from(Array(displayMovieCardCount - movies.length)).map((e, idx) => {
+              return <MovieCard key={idx} empty />
+            })
+          }
+        </div>
+        </>
+      }
     </div>
   )
 }
