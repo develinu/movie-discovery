8bbb09e704d11c71d9a6f932df9d210a943f2c32
LEEINWOO <49314154+develinu@users.noreply.github.com>
2023-03-09 21:53:22 +0900

    modify recommend user info

--- src/components/Recommend.js
+++ src/components/Recommend.js
@@ -6,15 +6,11 @@ import MovieCard from './MovieCard'
 
 
 const Recommend = () => {
-
   useEffect(() => {
     getMovies()
   }, [])
 
-  const name = ["이누", "대지", "가을", "동주"]
-  const [movie, setMovie] = useState({})
-  
-  let loading = true
+  const name = ["devinu"]
 
   return (
     <>
@@ -29,7 +25,7 @@ const RecommendBelt = ({ name }) => {
   return (
     <div className="recommend-belt">
       <div className="title">
-        {name}님이 좋아하는 영화 리스트
+        {name}님이 좋아 할만한 영화 리스트
       </div>
 
       <div className="recommend-list">
